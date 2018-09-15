6b93940a691e14d9a6c9b8d5e2d5eb89e5efe3f9
Markus Staab <markus.staab@redaxo.de>
2018-09-15 10:00:56 +0200

    initial commit

---
+++ src/AddonInstaller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Redaxo\Composer;
+
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller as BaseInstaller;
+
+class AddonInstaller extends BaseInstaller
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        list($owner, $packageName) = explode($package->getPrettyName(), 2);
+
+        return 'redaxo/src/addons/'.$packageName;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return 'redaxo-addon' === $packageType;
+    }
+}
